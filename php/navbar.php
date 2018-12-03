b24da3ab67250c0a884630675a8c04d62eed3698
Knockturne42 <dcl.thibauds@18pixel.fr>
2018-12-03 11:08:45 +0100

    nav dossier

---
+++ php/navbar.php
@@ -0,0 +1,11 @@
+<header>
+	<div class="container-fluid">
+		<ul class="nav navbar-nav navbar-fixed-bottom">
+			<li class="active"><a href="#">Accueil</a></li>
+		    <li><a href="#">Nouveautés</a></li>
+		    <li><a href="#">Top Films</a></li>
+		    <li><a href="#">Ajouter</a></li>
+		    <li><a href="#">Contact</a></li>
+		</ul>
+	</div>
+</header>
