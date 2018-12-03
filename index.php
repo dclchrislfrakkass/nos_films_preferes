8980458a41fefad4c1c5eeb66dc63fd6d9d524e4
Knockturne42 <dcl.thibauds@18pixel.fr>
2018-12-03 11:05:43 +0100

    form vide

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<main>
+		<?php
+			include 'php/form.php';
+		?>
+	</main>
+	<footer></footer>
+</body>
+</html>
---
+++ php/form.php
@@ -0,0 +1,28 @@
+<?php 
+
+echo '<form action="">';
+echo '<input type="text" value="" name="rechercheMotsCle" placeholder="Recherche">';
+echo '<div id="more">';
+echo '<input type="text" value="" name="rechercheNom" placeholder="Nom du Film">';
+mySelect('genre', 5);
+mySelect('annee', 6);
+echo '<input type="text" value="" name="rechercheRealisateur" placeholder="Realisateur">';
+echo '<input type="text" value="" name="rechercheActeur" placeholder="Acteur">';
+echo '<input type="text" value="" name="rechercheScenariste" placeholder="Scenariste">';
+echo '<input type="number" value="" name="duree" placeholder="duree">';
+echo '<input type="text" value="" name="utilisateur" placeholder="Utilisateur">';
+mySelect('pays', 3);
+echo '<input type="submit" value="recherchez">';
+echo '</div>';
+echo '</form>';
+
+function mySelect($name, $taille)
+{
+	echo '<select name="'.$name.'">';
+	echo '<option value="">'.$name.'</option>';
+	for ($i=0; $i < $taille; $i++) {
+		echo '<option value="">a voir</option>';
+	}
+	echo '</select>';
+}
+?>
