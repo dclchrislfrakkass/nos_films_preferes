85bd28c45e8db34e191d593d85565b8d1483c3a3
Knockturne42 <dcl.thibauds@18pixel.fr>
2018-12-03 16:11:10 +0100

    animation recherche

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 var rechercheInput = document.getElementById('recherche');
 var rechercheDiv = document.getElementById('more');
 var recherche = document.getElementsByTagName('form');
+var rechercheSubmit = document.getElementById('submit');
+rechercheInput.style.height = "100%";
 
 rechercheInput.addEventListener('click', function(){
-	recherche[0].style.margin = '0%';
-	rechercheDiv.style.display = 'flex';
+	recherche[0].style.marginTop = '0%';
+	recherche[0].style.height = '40%';
+	rechercheInput.style.height = "15%";
+	rechercheDiv.style.height = '80%';
 	setTimeout(function(){
-		rechercheDiv.style.height = '20%';
-		rechercheDiv.style.backgroundColor = 'blue';
-	},1000);
+		rechercheDiv.style.display = 'flex';
+	},500);
+});
+
+rechercheSubmit.addEventListener('click', function(){
+	rechercheDiv.style.height = '0%';
+	recherche[0].style.height = '5%';
+	rechercheInput.style.height = "100%";
+	setTimeout(function(){
+		rechercheDiv.style.height = '0%';
+		rechercheDiv.style.display = 'none';
+	},500);
 });
--- php/form.php
+++ php/form.php
@@ -1,24 +1,36 @@
 <?php 
 
-echo '<form action="">';
+echo '<form action="#">';
 echo '<input type="text" value="" name="rechercheMotsCle" placeholder="Recherche" id="recherche">';
 echo '<div id="more">';
-echo '<input type="text" value="" name="rechercheNom" placeholder="Nom du Film">';
+echo '<input type="text" value="" name="rechercheNom" placeholder="Nom du Film" class="formInp">';
 mySelect('genre', 5);
 mySelect('annee', 6);
-echo '<input type="text" value="" name="rechercheRealisateur" placeholder="Realisateur">';
-echo '<input type="text" value="" name="rechercheActeur" placeholder="Acteur">';
-echo '<input type="text" value="" name="rechercheScenariste" placeholder="Scenariste">';
-echo '<input type="number" value="" name="duree" placeholder="duree">';
-echo '<input type="text" value="" name="utilisateur" placeholder="Utilisateur">';
+echo '<input type="text" value="" name="rechercheRealisateur" placeholder="Realisateur" class="formInp">';
+echo '<input type="text" value="" name="rechercheActeur" placeholder="Acteur" class="formInp">';
+echo '<input type="text" value="" name="rechercheScenariste" placeholder="Scenariste" class="formInp">';
+echo '<input type="number" value="" name="duree" placeholder="duree" class="formInp">';
+echo '<input type="text" value="" name="utilisateur" placeholder="Utilisateur" class="formInp">';
 mySelect('pays', 3);
-echo '<input type="submit" value="recherchez">';
+echo '<input type="submit" value="recherchez" id="submit" class="formInp">';
 echo '</div>';
 echo '</form>';
 
+echo '<select name="trier" id="trier">';
+echo '<option value="">trier par</option>';
+echo '<option value="nomD">nom A-z</option>';
+echo '<option value="anneeD">annee 20xx-19xx</option>';
+echo '<option value="dureeD">duree 160-0</option>';
+echo '<option value="noteD">note 5-0</option>';
+echo '<option value="nomA">nom Z-a</option>';
+echo '<option value="anneeA">annee 19xx-20xx</option>';
+echo '<option value="dureeA">duree 0-160</option>';;
+echo '<option value="noteA">note 0-5</option>';
+echo '</select>';
+
 function mySelect($name, $taille)
 {
-	echo '<select name="'.$name.'">';
+	echo '<select name="'.$name.'" class="formInp">';
 	echo '<option value="">'.$name.'</option>';
 	for ($i=0; $i < $taille; $i++) {
 		echo '<option value="">a voir</option>';
