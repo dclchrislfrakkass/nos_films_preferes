f415f08e13563813112fe713328048a242680e6a
ChrisL_frakkass <dcl.chrisl@18pixel.fr>
2018-12-03 14:32:11 +0100

    some try

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Nos films préférés</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/style.css">
 </head>
@@ -14,14 +14,6 @@
 	<main>
 		<?php
             include 'php/form.php';
-
-            echo '<div class="billetFilm">';
-            echo '<div class="topLeft">'.(NOMDUFILM).' ('.(DUREE).' '.(ANNEE).')</div>';
-            echo '<img src="XXX" class="affiche"> alt="XXX"</div>';
-            echo '<div class="infos">Réalisateur : '.(REALISATEUR).' Acteurs: '.(ACTEUR).'</div>';
-            echo '<div class="resume">'.(RESUME).'</div>'; //version limitee
-            echo '<div class="resume">'.(RESUMEPLUS).'</div>'; //version full
-            echo '</div>';
 
 ?>
 	</main>
---
+++ php/billet.php
@@ -0,0 +1,14 @@
+<?php
+
+echo '<div class="billetFilm">';
+            echo '<div class="topLeft">'.(NOMDUFILM).' ('.(DUREE).' '.(ANNEE).')</div>';
+            echo '<img src="XXX" class="affiche"> alt="XXX"</div>';
+            echo '<div class="infos">Réalisateur : '.(REALISATEUR).' Acteurs: '.(ACTEUR).'</div>';
+            echo '<div class="resume">'.(RESUME).'</div>'; //version limitee
+            echo '<div class="resume">'.(RESUMEPLUS).'</div>'; //version full
+            echo '</div>';
+
+            ?>
+
+
+            https://image.flaticon.com/icons/svg/149/149668.svg
