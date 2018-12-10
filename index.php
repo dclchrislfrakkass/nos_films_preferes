80be25d9beaeda1b4fccb9ef6933c39e62a06e48
GEN-005 <gen-005@gen005-HP-ProBook-450-G3>
2018-12-10 12:26:22 +0100

    menu commit

--- index.php
+++ index.php
@@ -13,39 +13,36 @@
 </head>
 
 <body>
-
     <main id="main">
         <?php
 
         include 'php/form.php';
         ?>
     </main>
-<!--     
+ <footer>
     <div class="pos-f-t">
-    <div class="collapse" id="navbarToggleExternalContent">
-    <div class="bg-dark p-4">
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <ul class="nav navbar-nav">
+        <div class="collapse" id="navbarToggleExternalContent">
+            <div class="bg-dark p-4">
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid">
+                        <ul class="nav navbar-nav">
 
-                    <a href="#news" class="button button4">Nouveautés</a>
-                    <a href="#contact" class="button button4">Top Films</a>
-                    <a href="#about" class="button button4">Ajouter</a>
-                    <a href="#about" class="button button4">Contact</a>
+                            <a href="#news" class="button button4">Nouveautés</a>
+                            <a href="#contact" class="button button4">Top Films</a>
+                            <a href="#about" class="button button4">Ajouter</a>
+                            <a href="#about" class="button button4">Contact</a>
 
-
-                </ul>
+                        </ul>
+                    </div>
+                </nav>
             </div>
-        </nav>
-    </div>
-    </div>
+        </div>
         <nav class="navbar navbar-dark bg-dark">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
         </nav>
-    </div> -->
-<footer>
+    </div>
 </footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
