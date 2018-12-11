ec6b9bcc9c2356d94b9960de268ad2bb67419ec8
Knockturne42 <dcl.thibauds@18pixel.fr>
2018-12-11 16:56:11 +0100

    end of project no ended

--- php/insertFilm.php
+++ php/insertFilm.php
@@ -12,6 +12,19 @@
 	else
 		insertMyFilm($bdd);
 
+	function updateFilm($bdd, $table, $varDest, $idSearch, $tableSearch, $varCompar, $idCompart, $monGet)
+	{
+		$update = $bdd->query('UPDATE '.$table.' '.$varDest.'=(SELECT '.$idSearch.' FROM '.$tableSearch.' WHERE '.$varCompart.' LIKE "'.$monGet.'") WHERE '.$idCompart.' LIKE (SELECT Film.idFilm FROM Film WHERE Film.afficheFilm LIKE "'.$_GET['insertAffiche'].'")');
+	}
+
+	function insertFilm($bdd, $table, $nomSearch, $monGet, $tableDest)
+	{
+		$verifQuery = $bdd->query('SELECT * FROM '.$table.' WHERE '.$nomSearch.' LIKE "'.$monGet.'"');
+		if(!$verifQuery->fetch())
+			$insertIn = $bdd->query('INSERT INTO '.$table.' ('.$nomSearch.') VALUES ("'.$monGet.'")');
+		$insertIn = $bdd->query('INSERT INTO '.$tableDest.' (idFilm) SELECT idFilm FROM Film WHERE afficheFilm LIKE "'.$_GET['insertAffiche'].'"');
+	}
+
 	function insertMyFilm($bdd)
 	{
 		$realisateur = explode(' ', $_GET['insertRealisateur']);
@@ -23,26 +36,39 @@
 		$acteur5 = explode(' ', $_GET['insertActeur5']);
 
 		$insertInFilm = $bdd->query('INSERT INTO Film (resumeFilm, anneeFilm, dureeFilm, nomFilm, afficheFilm, lienBandeAnnonce) VALUES ("'.$_GET['insertResume'].'", "'.$_GET['insertAnnee'].'", "'.$_GET['insertDuree'].'", "'.$_GET['insertNom'].'", "'.$_GET['insertAffiche'].'", "'.$_GET['insertBandeAnnonce'].'")');
-		$verifQuery = $bdd->query('SELECT * FROM Pays WHERE nomPays LIKE "'.$_GET['insertPays'].'"');
-		if(!$verifQuery->fetch())
-			$insertInPays = $bdd->query('INSERT INTO Pays (nomPays) VALUES  ("'.$_GET['insertPays'].'")');
-		$insertInViens = $bdd->query('INSERT INTO vient_de (idPays) SELECT idPays FROM Pays WHERE nomPays LIKE "'.$_GET['insertPays'].'"');
-		$insertInViens = $bdd->query('INSERT INTO vient_de (idFilm) SELECT idFilm FROM Film WHERE afficheFilm LIKE "'.$_GET['insertAffiche'].'"');
 
-		$verifQuery = $bdd->query('SELECT * FROM genre WHERE nomGenre LIKE "'.$_GET['insertGenre'].'"');
-		if(!$verifQuery->fetch())
-			$insertInGenre = $bdd->query('INSERT INTO genre (nomGenre) VALUES  ("'.$_GET['insertGenre'].'")');
-		$insertInEst = $bdd->query('INSERT INTO est (idFilm) SELECT idFilm FROM Film WHERE afficheFilm LIKE "'.$_GET['insertAffiche'].'"');
-		$insertInEst = $bdd->query('INSERT INTO est (idGenre) SELECT idGenre FROM genre WHERE nomGenre LIKE "'.$_GET['insertGenre'].'"');
+		insertFilm($bdd, 'Pays', 'nomPays', $_GET['insertPays'], 'vient_de');
+		updateFilm($bdd, 'vient_de', 'idPays', 'Pays', 'nomPays', $_GET['insertPays']);
+		updateFilm($bdd, 'vient_de', 'idFilm', 'Film', 'nomFilm', $_GET['insertNom']);
+
+
+		// $verifQuery = $bdd->query('SELECT * FROM Pays WHERE nomPays LIKE "'.$_GET['insertPays'].'"');
+		// if(!$verifQuery->fetch())
+		// 	$insertInPays = $bdd->query('INSERT INTO Pays (nomPays) VALUES  ("'.$_GET['insertPays'].'")');
+		// $insertInViens = $bdd->query('INSERT INTO vient_de (idFilm) SELECT idFilm FROM Film WHERE afficheFilm LIKE "'.$_GET['insertAffiche'].'"');
+		// $insertInViens = $bdd->query('UPDATE vient_de idPays=(SELECT idPays FROM Pays WHERE nomPays LIKE "'.$_GET['insertPays'].'") WHERE idFilm LIKE (SELECT Film.idFilm FROM Film WHERE Film.afficheFilm LIKE "'.$_GET['insertAffiche'].'")');
+
+
+
+		insertFilm($bdd, 'genre', 'nomGenre', $_GET['insertGenre'], 'est');
+		updateFilm($bdd, 'est', 'idGenre', 'genre', 'nomGenre', $_GET['insertGenre']);
+		updateFilm($bdd, 'est', 'idFilm', 'Film', 'nomFilm', $_GET['insertNom']);
+		// $verifQuery = $bdd->query('SELECT * FROM genre WHERE nomGenre LIKE "'.$_GET['insertGenre'].'"');
+		// if(!$verifQuery->fetch())
+		// 	$insertInGenre = $bdd->query('INSERT INTO genre (nomGenre) VALUES  ("'.$_GET['insertGenre'].'")');
+		// $insertInEst = $bdd->query('INSERT INTO est (idFilm) SELECT idFilm FROM Film WHERE afficheFilm LIKE "'.$_GET['insertAffiche'].'"');
+		// $insertInEst = $bdd->query('UPDATE est idGenre=(SELECT idGenre FROM genre WHERE nomGenre LIKE "'.$_GET['insertGenre'].'") WHERE idFilm LIKE (SELECT Film.idFilm FROM Film WHERE Film.afficheFilm LIKE "'.$_GET['insertAffiche'].'")');
 
 		$keyWord = explode(' ', $_GET['insertKeyWord']);
-		var_dump($keyWord);
 		foreach ($keyWord as $key => $value) {
-			$verifQuery = $bdd->query('SELECT * FROM motCle WHERE motMotCle LIKE "'.$value.'"');
-			if(!$verifQuery->fetch())
-				$insertInKeyWord = $bdd->query('INSERT INTO motCle (motMotCle) VALUES  ("'.$value.'")');
-			$insertInDefinit = $bdd->query('INSERT INTO definit (idMotCle) SELECT motMotCle FROM motCle WHERE motMotCle LIKE "'.$value.'"');
-			$insertInDefinit = $bdd->query('INSERT INTO definit (idFilm) SELECT idFilm FROM Film WHERE afficheFilm LIKE "'.$_GET['insertAffiche'].'"');
+			insertFilm($bdd, 'motCle', 'motMotCle', $value, 'definit');
+			updateFilm($bdd, 'definit', 'idMotCle', 'motMotCle', 'motCle', $value);
+			updateFilm($bdd, 'definit', 'idFilm', 'Film', 'nomFilm', $_GET['insertNom']);
+			// $verifQuery = $bdd->query('SELECT * FROM motCle WHERE motMotCle LIKE "'.$value.'"');
+			// if(!$verifQuery->fetch())
+			// 	$insertInKeyWord = $bdd->query('INSERT INTO motCle (motMotCle) VALUES  ("'.$value.'")');
+			// $insertInDefinit = $bdd->query('INSERT INTO definit (idMotCle) SELECT motMotCle FROM motCle WHERE motMotCle LIKE "'.$value.'"');
+			// $insertInDefinit = $bdd->query('INSERT INTO definit (idFilm) SELECT idFilm FROM Film WHERE afficheFilm LIKE "'.$_GET['insertAffiche'].'"');
 		}
 		// $verifQuery = $bdd->query('SELECT * FROM Pays WHERE nomPays LIKE "'.$_GET['insertPays'].'"');
 		// if(!$verifQuery->fetch())
@@ -69,8 +95,8 @@
 			$insertInPersonne = $bdd->query('INSERT INTO personne (nomPersonne, prenomPersonne_personne, idPays) VALUES ("'.$personne[1].'", "'.$personne[0].'", "'.$idPays[0].'")');
 			var_dump('INSERT INTO personne (nomPersonne, prenomPersonne_personne, idPays) VALUES ("'.$personne[1].'", "'.$personne[0].'", "'.$idPays[0].'")');
 		}
-		$insertIn = $bdd->query('INSERT INTO '.$table.' (idFilm) SELECT idFilm FROM Film WHERE afficheFilm LIKE "'.$_GET['insertAffiche'].'"');
-		$insertIn = $bdd->query('UPDATE '.$table.' SET idPersonne= (SELECT idPersonne FROM personne WHERE nomPersonne LIKE "'.$personne[1].'" AND prenomPersonne_personne LIKE "'.$personne[0].'") WHERE idFilm LIKE (SELECT Film.idFilm FROM Film WHERE Film.afficheFilm LIKE "'.$_GET['insertAffiche'].'")');
+		$insertIn = $bdd->query('INSERT INTO '.$table.' (idFilm) VALUES (987654)');
+		// $insertIn = $bdd->query('UPDATE '.$table.' SET idPersonne= (SELECT idPersonne FROM personne WHERE nomPersonne LIKE "'.$personne[1].'" AND prenomPersonne_personne LIKE "'.$personne[0].'"), idFilm=(SELECT Film.idFilm FROM Film WHERE Film.afficheFilm LIKE "'.$_GET['insertAffiche'].'") WHERE idFilm LIKE 987654');
 
 		// var_dump('INSERT INTO '.$table.' (idFilm) SELECT idFilm FROM Film WHERE afficheFilm LIKE "'.$_GET['insertAffiche'].'"');
 	}
